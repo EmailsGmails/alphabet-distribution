149645a3503d4a3e25278225a47fa4bcd182645a
Emīls Goško <emils.gosko@gmail.com>
2017-08-17 09:16:26 +0300

    Add files via upload

---
+++ js/distributionScript.js
@@ -0,0 +1,41 @@
+function printAlphabetDistributionOfInputText() {
+	const distribution = parseInputTextByAlphabet();
+    document.getElementById("distribution").innerHTML = distribution;
+}
+
+function parseInputTextByAlphabet() {
+	const alphabet = 'abcdefghijklmnopqrstuvwxyz',
+        alphabetLetters = alphabet.split(''),
+        inputText = document.getElementById('textToParse').value,
+        distribution = getRelativeDistributionOfEachLetterInText(alphabetLetters, inputText);
+	return distribution;
+}
+
+function getRelativeDistributionOfEachLetterInText(alphabetLetters, inputText) {
+	var distribution = '',
+	    countOfEachLetter = []; 
+	countOfEachLetter = getCountOfEachLetterInText(alphabetLetters, inputText);
+	const sumOfAllLetters = getSumOfNumbersInArray(countOfEachLetter);
+    for (var i = 0; i < alphabetLetters.length; i++) {
+        singleLetterDistribution = (countOfEachLetter[i] / sumOfAllLetters * 100).toFixed(2);
+        distribution += alphabetLetters[i] + ': ' + singleLetterDistribution + '<br>';	
+    }
+	return distribution;
+}
+
+function getCountOfEachLetterInText(alphabetLetters, inputText) {
+	var countOfEachLetter = [];
+    for (var i = 0; i < alphabetLetters.length; i++) {
+        var letter = alphabetLetters[i];
+        countOfEachLetter.push(inputText.countOccurrenceOfSymbol(letter));
+    }
+	return countOfEachLetter;
+}
+
+String.prototype.countOccurrenceOfSymbol = function(symbol) { 
+    return this.length - this.replace(new RegExp(symbol,"gi"), '').length;
+}
+
+function getSumOfNumbersInArray(array) {
+    return array.reduce(function(a, b) { return a + b; }, 0);
+}
