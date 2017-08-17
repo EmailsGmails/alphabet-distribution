33a417be65a0a1864c85fd35bb60debf7f26f6c3
Emīls Goško <emils.gosko@gmail.com>
2017-08-17 09:22:53 +0300

    Add files via upload

--- js/distributionScript.js
+++ js/distributionScript.js
@@ -1,35 +1,35 @@
 function printAlphabetDistributionOfInputText() {
-	const distribution = parseInputTextByAlphabet();
+    const distribution = parseInputTextByAlphabet();
     document.getElementById("distribution").innerHTML = distribution;
 }
 
 function parseInputTextByAlphabet() {
-	const alphabet = 'abcdefghijklmnopqrstuvwxyz',
-        alphabetLetters = alphabet.split(''),
-        inputText = document.getElementById('textToParse').value,
-        distribution = getRelativeDistributionOfEachLetterInText(alphabetLetters, inputText);
-	return distribution;
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz',
+    alphabetLetters = alphabet.split(''),
+    inputText = document.getElementById('textToParse').value,
+    distribution = getRelativeDistributionOfEachLetterInText(alphabetLetters, inputText);
+    return distribution;
 }
 
 function getRelativeDistributionOfEachLetterInText(alphabetLetters, inputText) {
-	var distribution = '',
-	    countOfEachLetter = []; 
-	countOfEachLetter = getCountOfEachLetterInText(alphabetLetters, inputText);
-	const sumOfAllLetters = getSumOfNumbersInArray(countOfEachLetter);
+    var distribution = '',
+    countOfEachLetter = []; 
+    countOfEachLetter = getCountOfEachLetterInText(alphabetLetters, inputText);
+    const sumOfAllLetters = getSumOfNumbersInArray(countOfEachLetter);
     for (var i = 0; i < alphabetLetters.length; i++) {
         singleLetterDistribution = (countOfEachLetter[i] / sumOfAllLetters * 100).toFixed(2);
         distribution += alphabetLetters[i] + ': ' + singleLetterDistribution + '<br>';	
     }
-	return distribution;
+    return distribution;
 }
 
 function getCountOfEachLetterInText(alphabetLetters, inputText) {
-	var countOfEachLetter = [];
+    var countOfEachLetter = [];
     for (var i = 0; i < alphabetLetters.length; i++) {
         var letter = alphabetLetters[i];
         countOfEachLetter.push(inputText.countOccurrenceOfSymbol(letter));
     }
-	return countOfEachLetter;
+    return countOfEachLetter;
 }
 
 String.prototype.countOccurrenceOfSymbol = function(symbol) { 
